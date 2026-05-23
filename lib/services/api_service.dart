a3d5821012b9d40c459ab6f0d530cf2f33ec4b0c
eutanasi0 <benjaminflores4e@gmail.com>
2026-05-23 10:31:10 -0500

    render

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,21 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-
-  final String baseUrl = "http://10.0.2.2:8000";
+  final String baseUrl = "https://labo09-backend.onrender.com";
 
   Future<List<dynamic>> obtenerUsuarios() async {
-
-    final response =
-        await http.get(Uri.parse("$baseUrl/usuarios"));
+    final response = await http.get(Uri.parse("$baseUrl/usuarios"));
 
     return jsonDecode(response.body);
   }
 
   Future<List<dynamic>> obtenerPersonas() async {
-
-    final response =
-        await http.get(Uri.parse("$baseUrl/personas"));
+    final response = await http.get(Uri.parse("$baseUrl/personas"));
 
     return jsonDecode(response.body);
   }
